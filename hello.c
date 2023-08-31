414f7f1c22bd26df227fa38ae222ef89bd953da2
brett037 <bwiltgen2@huskers.unl.edu>
2023-08-31 12:49:17 -0500

    inital commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Edward Brett Wiltgen
+ *         bwiltgen2@huskers.unl.edu
+ * Date: 2023/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I love the School of Computing!\n");
+
+  return 0;
+}
